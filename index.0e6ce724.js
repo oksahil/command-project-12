c60cccc952fc1d5ba6a49f9c97eb611a4a57024f
oksahil <108234649+oksahil@users.noreply.github.com>
2022-09-11 09:24:34 +0000

    Deploying to gh-pages from @ oksahil/command-project-12@722a54b892421dcde7d27b4eaeed906abab2c997 🚀

---
+++ index.0e6ce724.js
@@ -0,0 +1,2 @@
+(()=>{const t={openContactsBtn:document.querySelector(".contacts-button"),closeContactsBtn:document.querySelector(".contacts-button"),contacts:document.querySelector(".contacts__menu")};function c(){t.contacts.classList.toggle("is-hidden")}t.openContactsBtn.addEventListener("click",c),t.closeContactsBtn.addEventListener("click",c)})();
+//# sourceMappingURL=index.0e6ce724.js.map
---
+++ index.6c877b22.js
@@ -0,0 +1,2 @@
+!function(){var t={openContactsBtn:document.querySelector(".contacts-button"),closeContactsBtn:document.querySelector(".contacts-button"),contacts:document.querySelector(".contacts__menu")};function n(){t.contacts.classList.toggle("is-hidden")}t.openContactsBtn.addEventListener("click",n),t.closeContactsBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.6c877b22.js.map
